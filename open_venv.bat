576dca7150d53ea13e151c0c0ff7202946dce285
Nostress767 <72526817+Nostress767@users.noreply.github.com>
2022-05-07 06:38:25 -0300

    Hide commands

--- open_venv.bat
+++ open_venv.bat
@@ -1,3 +1,4 @@
+@echo off
 if not exist external mkdir external
 cd external
 if not exist jna-5.11.0 curl -L --output jna-5.11.0.zip --url https://github.com/java-native-access/jna/archive/refs/tags/5.11.0.zip
@@ -10,4 +11,5 @@ if exist jdk-19.zip del jdk-19.zip
 cd ..
 
 @set PATH=%path%;%CD%\external\jdk-19\bin
+@echo on
 @cmd
